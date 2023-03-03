970a82b19f6701b45da4a59343eea42bb92d9e64
mahmutkaratay <mahmutkaratay61@gmail.com>
2023-03-04 00:03:52 +0300

    feat : content negotiation, xml output formatter, csv output formatter implementation

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -10,7 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
-builder.Services.AddControllers()
+builder.Services.AddControllers(config =>
+{
+    config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
+})
+    .AddCustomCsvFormatter()
+    .AddXmlDataContractSerializerFormatters()
     .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
     .AddNewtonsoftJson();
 
