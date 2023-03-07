2ffd462cc0b5161f8b5e7c035c544f2ecbc5bad8
mahmutkaratay <mahmutkaratay61@gmail.com>
2023-03-07 21:41:42 +0300

    feat: search implementation

---
+++ Repositories/EfCore/Extensions/BookRepositoryExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EfCore.Extensions
+{
+    public static class BookRepositoryExtensions
+    {
+        public static IQueryable<Book> FilterBooks(this IQueryable<Book> books, uint minPrice, uint maxPrcice) =>
+            books.Where(book =>
+            book.Price >= minPrice &&
+            book.Price <= maxPrcice);
+
+        public static IQueryable<Book> Search(this IQueryable<Book> books, string searchTerm)
+        {
+            if (string.IsNullOrEmpty(searchTerm))
+
+            {
+                return books;
+            }
+
+            var lowerCaseTerm = searchTerm.Trim().ToLower();
+
+
+            return books.Where(book => book.Title.ToLower().Contains(lowerCaseTerm));
+
+        }
+
+
+    }
+}
